e1de9d1ed882fb8d84cb9a9afd4bc2c698bb6b5c
OksanaNan <oksana.semka@gmail.com>
2021-08-09 20:23:27 +0300

    Responsive

--- script.js
+++ script.js
@@ -28,3 +28,53 @@ window.onscroll = () => {
     const nav = document.querySelector('#navbar');
     if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
   };
+
+
+
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll('.lock-padding');
+
+let unlock = true;
+
+const timeout = 800; 
+
+if (popupLinks.length > 0) {
+    for(let index = 0; index < popupLinks.length; index++) {
+        const popupLink = popupLinks[index]; 
+        popupLinks.addEventListener("click", function(e) {
+            const popupName = popupLink.getAttribute('href').replace('#','');
+            const curentPopup = document.getElementById(popupName);
+            popupOpen(curentPopup);
+            e.preventDefault(); 
+        });
+    }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if(popupCloseIcon.length > 0) {
+    for(let index = 0; index < popupCloseIcon.length; index++) {
+        const el = popupCloseIcon[index];
+        el.addEventListener('click', function (e) {
+            popupClose(el.closest('.popup'));
+            e.preventDefault();
+        });
+    }
+}
+
+
+function popupOpen(curentPopup) {
+    if (curentPopup && unlock) {
+        const popupActive = document.querySelector('.popup.open');
+        if(popupActive) {
+            popupClose(popupActive, false)
+        } else {
+            bodyLock();
+        }
+
+curentPopup.classList.add('open');
+curentPopup.addEventListener('click', function(e))
+
+
+    }
+} 
